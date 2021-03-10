0b3705f3fec64b4d7403b2103dbf08d00f4cc5d7
Miyuusagix <laurie.bouleau@outlook.fr>
2021-03-10 01:30:07 +0100

    Ajustement commentaires

--- constituer_un_equipage_wcs/src/Controller/HomeController.php
+++ constituer_un_equipage_wcs/src/Controller/HomeController.php
@@ -16,6 +16,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+
+     /**
+     * Affichage liste de l'équipage & ajout des nouveaux membres en BDD
+     *
+     * @Route("/home", name="home", methods={"GET", "POST"})
+     */
+    public function index(Request $request, CrewRepository $crewRepository)
+    {
+        // Nouvelle entité Crew
+        $crew = new Crew();
+
+        // Form basé sur CrewType et l'entité Crew
+        $form = $this->createForm(CrewType::class, $crew);
+
+        // Prise en charge de la requête
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On stocke le l'équipier en base
+            $em = $this->getDoctrine()->getManager();
+            // Le $crew a été déjà mis à jour par le form
+            $em->persist($crew);
+            $em->flush();
+
+            // Flash
+            $this->addFlash('success', 'Crew ajouté.');
+        }
+
+        // Select all crews ordered by name ASC
+        $crew = $crewRepository->findBy([], ['name' => 'ASC']);
+
+        // On affiche le form
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+            'crew' => $crew,
+        ]);
+    }
+
+
     /**
      * Affichage liste de l'équipage
      * 
@@ -34,50 +73,15 @@ class HomeController extends AbstractController
     }
     */
 
-     /**
-     * Affichage liste de l'équipage
-     *
-     * @Route("/home", name="home", methods={"GET", "POST"})
-     */
-    public function index(Request $request, CrewRepository $crewRepository)
-    {
-        // Nouvelle entité Job
-        $crew = new Crew();
 
-        // Form basé sur JobType et l'entité Job
-        $form = $this->createForm(CrewType::class, $crew);
-
-        // Prise en charge de la requête
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // On stocke le job en base
-            $em = $this->getDoctrine()->getManager();
-            // Le $job a été déjà mis à jour par le form
-            $em->persist($crew);
-            $em->flush();
-
-            // Flash
-            $this->addFlash('success', 'Crew ajouté.');
-        }
-
-        // Select all jobs ordered by name ASC
-        $crew = $crewRepository->findBy([], ['name' => 'ASC']);
-
-        // On affiche le form
-        return $this->render('home/index.html.twig', [
-            'form' => $form->createView(),
-            'crew' => $crew,
-        ]);
-    }
 
     /**
      * Enregistrer un nouvel équipier en BDD
      * 
      * @Route("/add", name="add")
      */
-    public function create(EntityManagerInterface $entityManagerInterface, Request $request): Response
-    {
+    // public function create(EntityManagerInterface $entityManagerInterface, Request $request): Response
+    // {
 //         $crew = new Crew();
 //         $form = $this->createFormBuilder($crew)
 //             ->add(’name’)
@@ -87,28 +91,28 @@ class HomeController extends AbstractController
 // ’form’ => $form->createView(),
 // ]);
 
-        $crew = new Crew;
+    //     $crew = new Crew;
 
-        $form = $this->createForm(CrewType::class, $crew);
+    //     $form = $this->createForm(CrewType::class, $crew);
 
-        //le form gere la requete
-        $form->handleRequest($request);
+    //     //le form gere la requete
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+    //     if ($form->isSubmitted() && $form->isValid()) {
 
-            // Note: on a délégué la gestion du slug au Listener
+    //         // Note: on a délégué la gestion du slug au Listener
 
-            $entityManagerInterface->persist($crew);
+    //         $entityManagerInterface->persist($crew);
     
-            $entityManagerInterface->flush();
+    //         $entityManagerInterface->flush();
 
-            $this->addFlash('success', 'Membre ajouté !');
-        }
+    //         $this->addFlash('success', 'Membre ajouté !');
+    //     }
 
-        return $this->render('home/index.html.twig', [
-            // 'controller_name' => 'HomeController',
-        ]);
-    }
+    //     return $this->render('home/index.html.twig', [
+    //         // 'controller_name' => 'HomeController',
+    //     ]);
+    // }
 
    
 
@@ -118,68 +122,68 @@ class HomeController extends AbstractController
      * @Route("/new/crew", name="newcrew", methods={"POST","PUT","PATCH"})
      */
 
-   public function newCrew( Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManagerInterface): Response
-   {
+//    public function newCrew( Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManagerInterface): Response
+//    {
        
-       $jsonContent = $request->getContent();
+//        $jsonContent = $request->getContent();
        
-       $newCrew = $serializer->deserialize($jsonContent, Crew::class, 'json' );
+//        $newCrew = $serializer->deserialize($jsonContent, Crew::class, 'json' );
       
-       $errors = $validator->validate($newCrew);
+//        $errors = $validator->validate($newCrew);
 
-       // Si nombre d'erreur > 0 alors on renvoie une erreur
-       if (count($errors) > 0) {
-           // On retourne le tableau d'erreurs en Json au front 
-           return $this->json('ça ne fonctionne pas', Response::HTTP_UNPROCESSABLE_ENTITY);
-       }
+//        // Si nombre d'erreur > 0 alors on renvoie une erreur
+//        if (count($errors) > 0) {
+//            // On retourne le tableau d'erreurs en Json au front 
+//            return $this->json('ça ne fonctionne pas', Response::HTTP_UNPROCESSABLE_ENTITY);
+//        }
 
-       $entityManagerInterface->persist($newCrew);
+//        $entityManagerInterface->persist($newCrew);
 
-       $entityManagerInterface->flush();
+//        $entityManagerInterface->flush();
 
-       $this->addFlash('success', 'Membre ajouté !');
+//        $this->addFlash('success', 'Membre ajouté !');
 
-    //    return $this->json(['message' => 'L\'équipier a été créé'], Response::HTTP_CREATED);
+//     //    return $this->json(['message' => 'L\'équipier a été créé'], Response::HTTP_CREATED);
 
-       return $this->render('home/index.html.twig');
+//        return $this->render('home/index.html.twig');
 
-   }
+//    }
 
 
-   /**
-     * Ajouter un équipier
-     *
-     * @Route("/crew/add", name="crew_add", methods={"GET", "POST"})
-     */
-    public function add(Request $request)
-    {
-        // Nouvelle entité Job
-        $crew = new Crew();
+//    /**
+//      * Ajouter un équipier
+//      *
+//      * @Route("/crew/add", name="crew_add", methods={"GET", "POST"})
+//      */
+//     public function add(Request $request)
+//     {
+//         // Nouvelle entité Job
+//         $crew = new Crew();
 
-        // Form basé sur JobType et l'entité Job
-        $form = $this->createForm(CrewType::class, $crew);
+//         // Form basé sur JobType et l'entité Job
+//         $form = $this->createForm(CrewType::class, $crew);
 
-        // Prise en charge de la requête
-        $form->handleRequest($request);
+//         // Prise en charge de la requête
+//         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // On stocke le job en base
-            $em = $this->getDoctrine()->getManager();
-            // Le $job a été déjà mis à jour par le form
-            $em->persist($crew);
-            $em->flush();
+//         if ($form->isSubmitted() && $form->isValid()) {
+//             // On stocke le job en base
+//             $em = $this->getDoctrine()->getManager();
+//             // Le $job a été déjà mis à jour par le form
+//             $em->persist($crew);
+//             $em->flush();
 
-            // Flash
-            $this->addFlash('success', 'équipier ajouté.');
+//             // Flash
+//             $this->addFlash('success', 'équipier ajouté.');
 
-            // // Retour vers la liste des Jobs
-            // return $this->redirectToRoute('back_job_show', ['id' => $job->getId()]);
-        }
+//             // // Retour vers la liste des Jobs
+//             // return $this->redirectToRoute('back_job_show', ['id' => $job->getId()]);
+//         }
 
-        // On affiche le form
-        return $this->render('home/index.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
+//         // On affiche le form
+//         return $this->render('home/index.html.twig', [
+//             'form' => $form->createView(),
+//         ]);
+//     }
 
 }
